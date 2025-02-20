0a74da41ab2bef83baf8db30f96c605452c1be10
BatoolShaderma <batooshaderma13@gmail.com>
2025-02-20 22:46:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 let students=[];
 function createStudent(name,age,mark){
 students.push({name,age,mark});
-    
 }
 createStudent("Ahmad", 19 ,80);
 createStudent("Noor",20 ,70);
 createStudent("Batool",19 ,90);
 createStudent("Adam",20 ,40);
 createStudent("Zaid",19 ,45);
-
 
 let successStudents=[];
 let failedStudents=[];
@@ -17,12 +15,11 @@ for(let student of students){
     successStudents.push(student);
 }
 
-    else
+ else
   {  
     failedStudents.push(student);
   }
-
-
 }
+
 console.log("Success Students:" , successStudents);
 console.log("Failed Students:" , failedStudents);
